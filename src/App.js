800b9a6971d14645fbacb1fb7381797dd3c8a738
Zyh <zhangyinghaoweb@163.com>
2019-01-23 17:34:00 +0800

    暴露webpack配置文件，项目基本环境搭建

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Button } from 'antd'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button>3232</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
