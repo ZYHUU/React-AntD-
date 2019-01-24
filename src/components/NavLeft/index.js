9788250cad2862d10c90ac65aa10d05dfa62f8ab
Zyh <zhangyinghaoweb@163.com>
2019-01-24 16:30:06 +0800

    完成菜单组件开发

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,9 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Menu, Icon, Switch } from 'antd';
+import MeanConfig from '../../config/menuConfig';
+import './index.less';
 
+
+const SubMenu = Menu.SubMenu;
 class NavLeft extends Component{
+    constructor(props) {
+        super(props);
+        this.renderMenu = this.renderMenu.bind(this);
+    }
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(MeanConfig);
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    // 菜单渲染
+    renderMenu(data) {
+        return data.map((item) => {
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children) }
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+        })
+    }
     render() {
         return (
-            <div>NavLeft</div>
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt="" />
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu
+                    theme="dark"
+                >
+                    {this.state.menuTreeNode}
+                </Menu>
+            </div>
         )
     }
 }
