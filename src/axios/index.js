af38bbd083fabb3085c3398e7dda82fbd31b15bc
zyh <zhangyinghaoweb@163.com>
2019-01-24 19:52:51 +0800

    完成header部分

---
+++ src/axios/index.js
@@ -0,0 +1,17 @@
+import JsonP from 'jsonp';
+
+export default class Axios{
+    static jsonp(options) {
+        return new Promise((reslove, reject) => {
+            JsonP(options.url, {
+                param: 'callback'
+            }, function (err, response) {
+                if (response.status == 'success') {
+                    reslove(response);
+                } else {
+                    reject(response.message)
+                }
+            })
+        })
+    }
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import './index.less';
 import Util from '../../utils/utils'
+import axios from '../../axios'
 class Header extends Component{
     constructor(props){
         super(props);
@@ -19,7 +20,23 @@ class Header extends Component{
             this.setState({
                 sysTime
             })
-        },1000)
+        }, 1000)
+        this.getWeatherAPIData();
+    }
+
+    getWeatherAPIData() {
+        let city = '深圳';
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res) => {
+            if (res.status == 'success') {
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl: data.dayPictureUrl,
+                    weather: data.weather
+                })
+            }
+        })
     }
     render() {
         return(
@@ -36,7 +53,12 @@ class Header extends Component{
                     </Col>
                     <Col span={20} className="weather">
                         <span className="date">{this.state.sysTime}</span> 
-                        <span className="weater-detail">大到暴雨</span>   
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt="" />
+                        </span>   
+                        <span className="weather-detail">
+                            {this.state.weather}
+                        </span>
                     </Col>
                 </Row>
             </div>
